d3f9ea16be634bbe6475961567ff77fb15e99ead
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2019-12-24 15:45:08 -0300

    feat(awe): Add look of i am happy with it

--- routes/awe.js
+++ routes/awe.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 
 router.get('/look-of-awe-and-joy', (req, res) => res.status(200).json({ look: '{´◕ ◡ ◕｀}'}))
 router.get('/derp-look', (req, res) => res.status(200).json({ look: '¯\(◉◡◔)/¯'}))
+router.get('/look-of-i-am-happy-with-it', (req, res) => res.status(200).json({ look: 'ʘ‿ʘ'}))
 
 module.exports = router
