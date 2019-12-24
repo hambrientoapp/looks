619e3c91217ec2ef6e0fa6361091046357abe36f
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2019-12-24 15:43:02 -0300

    feat(awe): Add derp look path

--- routes/awe.js
+++ routes/awe.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
 router.get('/look-of-awe-and-joy', (req, res) => res.status(200).json({ look: '{´◕ ◡ ◕｀}'}))
+router.get('/derp-look', (req, res) => res.status(200).json({ look: '¯\(◉◡◔)/¯'}))
 
 module.exports = router
